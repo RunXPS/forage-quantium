c85624b404458f85de562f78a4e184107904e048
RunXPS <krasinski.j.ryan@gmail.com>
2024-08-24 16:45:47 -0400

    Finishing the Program

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Quantium starter repo
 This repo contains everything you need to get started on the program! Good luck!
+
+# Rationale
+I followed along with this forage program to get a grasp on what it is, with some hopes that I will be able to gain some insight on the software engineering industry.
+
+# Reflection
+I found this program very fun, even if it was fairly simplistic. I look forward to working with pandas more
+
+Also, I hope to be able to finish 'task6' sometime in the future.
--- test_task4.py
+++ test_task4.py
@@ -1,4 +1,5 @@
 from task4 import update_graph
+from task4 import app
 import pytest
 
 from selenium import webdriver
@@ -44,3 +45,18 @@ def test_elements_present():
     time.sleep(5)
     driver.quit()
     assert header != [] and graph != [] and radio != []
+
+
+def test_header_exists(dash_duo):
+    dash_duo.start_server(app)
+    dash_duo.wait_for_element("#header", timeout=10)
+
+
+def test_visualization_exists(dash_duo):
+    dash_duo.start_server(app)
+    dash_duo.wait_for_element("#visualization", timeout=10)
+
+
+def test_region_picker_exists(dash_duo):
+    dash_duo.start_server(app)
+    dash_duo.wait_for_element("#region_picker", timeout=10)
